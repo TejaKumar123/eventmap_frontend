2952d1ad9e23194d559608338eddb60145bac57b
TejaKumar123 <inceptioncom414@gmail.com>
2024-12-13 17:12:17 +0530

    small changes

--- src/components/basic/admin/Admin_speaker_row.jsx
+++ src/components/basic/admin/Admin_speaker_row.jsx
@@ -1,18 +1,23 @@
 import Delete from "@mui/icons-material/Delete"
 import { v4 as uuid } from "uuid"
 
-const Admin_speaker_row = ({ icon, name, username, email, sessions, date }) => {
+const Admin_speaker_row = ({ icon, name, username, email, sessions, date, onClick }) => {
+
+	const deleteParticipant = async () => {
+		onClick(email);
+	}
+
 	return (
 		<>
 			<tr className="text-[95%]" key={uuid()}>
-				<td className="p-[5px] text-center">{icon}</td>
+				<td className="p-[5px] text-center flex items-center justify-center">{icon}</td>
 				<td className="p-[2px_0px] text-center">{name}</td>
-				{/* <td className="p-[2px_0px] text-center">{username}</td> */}
+				<td className="p-[2px_0px] text-center">{username}</td>
 				<td className="p-[2px_0px] text-center">{email}</td>
 				<td className="text-center">{sessions}</td>
 				<td className="p-[2px_0px] text-center">{date}</td>
 				<td className="p-[2px_0px] text-center">
-					<Delete className="cursor-pointer" title="Delete this user" />
+					<Delete className="cursor-pointer" title="Delete this user" onClick={deleteParticipant} />
 				</td>
 			</tr>
 		</>
--- src/components/major/admin/Admin_speakers.jsx
+++ src/components/major/admin/Admin_speakers.jsx
@@ -2,24 +2,35 @@ import Person from "@mui/icons-material/Person"
 import Admin_speaker_row from "../../basic/admin/Admin_speaker_row"
 import AccountCircle from "@mui/icons-material/AccountCircle"
 import Templatediv from "../../basic/other/Templatediv"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { setHeading } from "../../../store/slices/otherSlice"
-import { userData } from "../../../assets/data/data"
 import moment from "moment"
 import { findUser } from "../../../store/slices/userSlice"
 import { v4 as uuid } from "uuid"
 
+
 const Admin_speakers = () => {
 
 	const dispatch = useDispatch();
+	const [userData, setUserData] = useState([]);
 
 	const fetchSpeakers = async () => {
 		let criteria = { role: "speaker" };
 		let projection = {};
 		dispatch(findUser({ criteria, projection })).then(action => {
-			console.log(action?.payload);
+			/* console.log(action?.payload); */
+			if (action?.payload?.status == "ok") {
+				setUserData(action?.payload?.data);
+			}
+			else {
+				setUserData([]);
+			}
 		})
+	}
+
+	const deleteParticipant = async (email) => {
+		alert(email);
 	}
 
 	useEffect(() => {
@@ -30,24 +41,38 @@ const Admin_speakers = () => {
 	return (
 		<>
 			<Templatediv>
-				<table className="w-full" key={uuid()}>
-					<tr className="" key={uuid()}>
-						<td className="p-[5px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]"><Person /></td>
-						<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Name</td>
-						{/* <td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Username</td> */}
-						<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Email</td>
-						<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Total Sessions</td>
-						<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Created at</td>
-						<td className="p-[2px_0px] text-center font-bold">Operations</td>
-					</tr>
-					{userData.map((data) => {
-						if (data["role"] == "speaker") {
-							return (
-								< Admin_speaker_row icon={< AccountCircle />} name={data["name"]} username={"TejaKumar123"} email={data["email"]} sessions={10} date={moment(data["createdAt"]).format("DD-MM-YYYY")} />
-							)
-						}
-					})}
-				</table>
+				{userData.length != 0 ?
+					<table className="w-full" key={uuid()}>
+						<tr className="" key={uuid()}>
+							<td className="p-[5px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]"><Person /></td>
+							<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Name</td>
+							<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Username</td>
+							<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Email</td>
+							<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Total Sessions</td>
+							<td className="p-[2px_0px] text-center font-bold border-r-[rgba(255,255,255,0.3)] border-r-[1px]">Created at</td>
+							<td className="p-[2px_0px] text-center font-bold">Operations</td>
+						</tr>
+						{userData.map((data) => {
+							if (data?.status == "fulfilled" && data?.value?.["role"] == "speaker") {
+								return (
+									< Admin_speaker_row
+										icon={data?.value?.profileimage ?
+											<img src={data?.value?.profileimage} className="w-[25px] rounded-[100%]" />
+											: < AccountCircle />}
+										name={data?.value?.["name"]}
+										username={data?.value?.["username"]}
+										email={data?.value?.["email"]}
+										sessions={data?.value?.sessionCount}
+										date={moment(data?.value?.["createdAt"]).format("DD-MM-YYYY")}
+										onClick={deleteParticipant}
+									/>
+								)
+							}
+						})}
+					</table>
+					:
+					<p className="text-center opacity-[0.5] text-[150%]">There are no participants in eventmap</p>
+				}
 			</Templatediv>
 		</>
 	)
