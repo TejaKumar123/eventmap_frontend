459bc51b50e4f6a2aa8ff691b9ae132006879738
TejaKumar123 <inceptioncom414@gmail.com>
2024-12-18 18:56:40 +0530

    small changes

--- src/components/major/participant/Participant_session.jsx
+++ src/components/major/participant/Participant_session.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom"
-import { Suspense } from "react"
 
 const Participant_session = () => {
 	return (
 		<div className="w-[100%] h-auto">
-			<Suspense fallback={<div>Loading Page...</div>} >
-				<Outlet />
-			</Suspense>
+			<Outlet />
 		</div>
 	)
 }
